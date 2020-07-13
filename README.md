f11321fb4012892ed3cefcf53f30fd321916b65a
gaomingyang <gaomingyang@migu.cn>
2020-07-13 23:07:24 +0800

    添加了 https://learnopengl-cn.github.io/01%20Getting%20started/05%20Shaders/ 学习的上半部分。

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 苹果 Mac 电脑，Clion 开发IDE，学习OpenGL 的工程
 
 [配置参考链接](https://www.yuque.com/yidenghou/qe4lyx/qpddw6)
+
+### 参考学习网页
+
+[LearnOpenGL](https://learnopengl-cn.github.io/01%20Getting%20started/05%20Shaders/)
--- cmake-build-debug/CMakeFiles/OpenglLearning.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/OpenglLearning.dir/DependInfo.cmake
@@ -1,8 +1,20 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/gaomingyang/Documents/MG/MIGUWorkSpace/OpenGLWorkSpace/CodeTest/OpenglLearning/glad/src/glad.c" "/Users/gaomingyang/Documents/MG/MIGUWorkSpace/OpenGLWorkSpace/CodeTest/OpenglLearning/cmake-build-debug/CMakeFiles/OpenglLearning.dir/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/glew/2.1.0_1/include/GL"
+  "/usr/local/Cellar/glfw/3.3.2/include/GLFW"
+  "../glad/include"
+  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/Users/gaomingyang/Documents/MG/MIGUWorkSpace/OpenGLWorkSpace/CodeTest/OpenglLearning/main.cpp" "/Users/gaomingyang/Documents/MG/MIGUWorkSpace/OpenGLWorkSpace/CodeTest/OpenglLearning/cmake-build-debug/CMakeFiles/OpenglLearning.dir/main.cpp.o"
   )
@@ -12,6 +24,7 @@ set(CMAKE_CXX_COMPILER_ID "AppleClang")
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "/usr/local/Cellar/glew/2.1.0_1/include/GL"
   "/usr/local/Cellar/glfw/3.3.2/include/GLFW"
+  "../glad/include"
   )
 
 # Targets to which this target links.

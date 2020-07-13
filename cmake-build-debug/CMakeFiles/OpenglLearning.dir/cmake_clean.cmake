2f59ee94aac8719ae174c291b318547318950c50
gaomingyang <gaomingyang@migu.cn>
2020-07-13 19:25:55 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/OpenglLearning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenglLearning.dir/main.cpp.o"
+  "OpenglLearning"
+  "OpenglLearning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenglLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
